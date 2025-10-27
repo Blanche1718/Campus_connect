7dc262cfddab223eb20a1cfe30688d63f259d311
Blanche1718 <blanchekf16@gmail.com>
2025-10-27 22:40:31 +0100

    Initialisation du projet

---
+++ campus_connect/database/migrations/2025_10_27_212515_create_annonces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->text('contenu');
+            $table->foreignId('categorie_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('auteur_id')->constrained('users')->onDelete('cascade');
+            $table->dateTime('date_publication')->useCurrent();
+            $table->dateTime('date_evenement')->nullable();
+            $table->foreignId('salle_id')->nullable()->constrained('salles')->onDelete('set null');
+            $table->foreignId('equipement_id')->nullable()->constrained('equipements')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('annonces');
+    }
+
+};
